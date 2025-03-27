227a24911116f5f1419a0ed905eeea70424ae807
Ajknight121 <aknig2@uic.edu>
2025-03-27 02:50:38 -0500

    Customers move to shelf,register, then door

--- Assets/!_ProjectMain/Scripts/CustomerMovement.cs
+++ Assets/!_ProjectMain/Scripts/CustomerMovement.cs
@@ -4,7 +4,15 @@ using UnityEngine.AI;
 public class CustomerMovement : MonoBehaviour
 {
     public enum State {IDLE, MOVING}
+
+    public enum Goal
+    {
+        SHOP,
+        PURCHASE,
+        EXIT
+    }
     public State state;
+    public Goal goal = Goal.PURCHASE;
     public float moveTime;
     public float maxMoveTime;
     public float waitTime;
@@ -12,19 +20,29 @@ public class CustomerMovement : MonoBehaviour
     public float speed;
     public NavMeshAgent agent;
     
+    public Transform currentDestination;
     public Transform shelveLocation;
+    public Transform exitLocation;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         state = State.MOVING;
-        shelveLocation = CustomerSpawner.GetShelveLocation();
+        goal = Goal.SHOP;
+        currentDestination = CustomerSpawner.GetShelveLocation();
         agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float radius = 2f;
+        float sqrRadius = radius * radius;
+        if ((transform.position - currentDestination.position).sqrMagnitude <= sqrRadius)
+        {
+            DestinationReached();
+        }
+        
         if (state == State.MOVING && moveTime > maxMoveTime)
         {
             waitTime = 0;
@@ -35,16 +53,39 @@ public class CustomerMovement : MonoBehaviour
             state = State.MOVING;
             moveTime = 0;
         }
-        if (state == State.MOVING)
+        switch (state)
         {
-            agent.SetDestination(shelveLocation.position);
-            // transform.Translate(0, 0, speed * Time.deltaTime);
-            moveTime += 1 * Time.deltaTime;
+            case State.MOVING:
+                agent.SetDestination(currentDestination.position);
+                moveTime += 1 * Time.deltaTime;
+                break;
+            case State.IDLE:
+                agent.SetDestination(transform.position);
+                waitTime += 1 * Time.deltaTime;
+                break;
         }
-        if (state == State.IDLE)
+    }
+
+    void DestinationReached()
+    {
+        // play animation
+        // stop movement for animation time
+        state = State.IDLE;
+        waitTime = 0;
+        switch (goal)
         {
-            agent.SetDestination(transform.position);
-            waitTime += 1 * Time.deltaTime;
+            // Choose next goal (may reroll another of same goal type
+            case Goal.SHOP:
+                goal = Goal.PURCHASE;
+                currentDestination = CustomerSpawner.GetRegisterLocation();
+                break;
+            case Goal.PURCHASE:
+                goal = Goal.EXIT;
+                currentDestination = CustomerSpawner.GetEntranceLocation();
+                break;
+            case Goal.EXIT:
+                Destroy(gameObject);
+                break;
         }
     }
 }
