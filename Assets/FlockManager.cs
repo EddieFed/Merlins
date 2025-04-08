257231c1e365e0e930efc22115d485c0180eb86f
Ajknight121 <97642775+Ajknight121@users.noreply.github.com>
2025-04-08 18:19:53 -0500

    progress on flocking

---
+++ Assets/FlockManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+    public static FlockManager FM;
+    public GameObject batPrefab;
+    public int numBats = 20;
+    public GameObject[] allBats;
+    public Vector3 flyLimits = new Vector3(5, 5, 5); // L W H container 
+    
+    [Header ("Bat Settings")]
+    [Range(0.0f, 5.0f)]
+    public float minSpeed = 1.0f;
+    [Range(0.0f, 5.0f)]
+    public float maxSpeed = 2.0f;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        allBats = new GameObject[numBats];
+        foreach (var bat in allBats)
+        {
+            Vector3 pos = this.transform.position + new Vector3(Random.Range(-flyLimits.x, flyLimits.x),
+                                                                Random.Range(-flyLimits.y, flyLimits.y), 
+                                                                Random.Range(-flyLimits.z, flyLimits.z));
+            Instantiate(batPrefab, pos, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
