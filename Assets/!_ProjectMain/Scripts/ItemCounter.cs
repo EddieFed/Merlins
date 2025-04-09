4eb398565ea64c185be702b7daf427ae1b6d8ddc
Daniel Barajas <danbarajas7@gmail.com>
2025-04-09 00:05:26 -0500

    Updated restocking function and added Slime AI

--- Assets/!_ProjectMain/Scripts/ItemCounter.cs
+++ Assets/!_ProjectMain/Scripts/ItemCounter.cs
@@ -1,18 +1,23 @@
 using System;
+using TMPro;
 using UnityEngine;
 
 public class ItemCounter : MonoBehaviour
 {
-    private int itemCount = 0;
+    public int itemCount = 10;
+    public int maxItems = 10;
+    public TextMeshPro stockCountText;
     
     private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && itemCount < maxItems)
             itemCount++;
     }
 
     private void Update()
     {
+        stockCountText.text = itemCount + "/" + maxItems;
+        
         if (itemCount == 0)
             GetComponent<MeshRenderer>().material.color = Color.red;
         else
