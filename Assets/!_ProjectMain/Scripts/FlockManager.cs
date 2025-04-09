f40892f47e3abf876273ca5a39d52d9da0429510
Ajknight121 <97642775+Ajknight121@users.noreply.github.com>
2025-04-08 21:35:33 -0500

    fly flocking

--- Assets/!_ProjectMain/Scripts/FlockManager.cs
+++ Assets/!_ProjectMain/Scripts/FlockManager.cs
@@ -7,6 +7,7 @@ public class FlockManager : MonoBehaviour
     public int numBats = 20;
     public GameObject[] allBats;
     public Vector3 flyLimits = new Vector3(5, 5, 5); // L W H container 
+    public Vector3 goalPosition = Vector3.zero;
     
     [Header ("Bat Settings")]
     [Range(0.0f, 5.0f)]
@@ -15,17 +16,23 @@ public class FlockManager : MonoBehaviour
     public float maxSpeed = 2.0f;
     [Range(1.0f, 10.0f)]
     public float neighbourDistance = 2.0f;
+    [Range(1.0f, 5.0f)]
+    public float avoidDistance = 1.0f;
+    [Range(1.0f, 5.0f)]
+    public float rotationSpeed = 4.0f;
+
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         allBats = new GameObject[numBats];
-        foreach (var bat in allBats)
+        for (int i = 0; i < numBats; i++)
         {
             Vector3 pos = this.transform.position + new Vector3(Random.Range(-flyLimits.x, flyLimits.x),
-                                                                Random.Range(-flyLimits.y, flyLimits.y), 
-                                                                Random.Range(-flyLimits.z, flyLimits.z));
-            Instantiate(batPrefab, pos, Quaternion.identity);
+                Random.Range(-flyLimits.y, flyLimits.y), 
+                Random.Range(-flyLimits.z, flyLimits.z));
+            allBats[i] = Instantiate(batPrefab, pos, Quaternion.identity);
+            allBats[i].transform.SetParent(transform); // Parent to this script’s GameObject
         }
 
         FM = this;
@@ -34,5 +41,13 @@ public class FlockManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Change set the goal direction
+        if (Random.Range(0, 100) < 10)
+        {
+            goalPosition = this.transform.position + new Vector3(Random.Range(-flyLimits.x, flyLimits.x),
+                Random.Range(-flyLimits.y, flyLimits.y), 
+                Random.Range(-flyLimits.z, flyLimits.z));
+            
+        }
     }
 }
