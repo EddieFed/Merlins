5d1c3d7f098f5e10212b07e82d5ea48ef28ea14f
Ajknight121 <97642775+Ajknight121@users.noreply.github.com>
2025-04-27 19:32:59 -0500

    Player interact with shelves

---
+++ Assets/!_ProjectMain/Scripts/InteractionController.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using __ProjectMain.Scripts;
+using UnityEngine;
+
+public class InteractionController : MonoBehaviour
+{
+    public KeyCode interactKey = KeyCode.E;
+    [SerializeField] public List<IInteractable> interactables = new List<IInteractable>();
+
+    private void InteractCurrent()
+    {
+        interactables[0].Interact(this.gameObject);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(interactKey)) // Example: press E to interact
+        {
+            Debug.Log("Interact");
+            if (interactables.Count > 0)
+            {
+                Debug.Log("interacting");
+                InteractCurrent();
+            }
+        }
+    }
+
+    public void InteractEnter(Collider other)
+    {
+        IInteractable interactable = other.GetComponent<IInteractable>();
+        if (interactable != null && !interactables.Contains(interactable))
+        {
+            Debug.Log(interactables.Count);
+            interactables.Add(interactable);
+        }
+    }
+
+    public void InteractExit(Collider other)
+    {
+        IInteractable interactable = other.GetComponent<IInteractable>();
+        if (interactable != null && interactables.Contains(interactable))
+        {
+            Debug.Log(interactables.Count);
+            interactables.Remove(interactable);
+        }
+    }
+}
